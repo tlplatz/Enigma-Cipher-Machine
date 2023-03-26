a7b318b50ba4830f1272f7d8f16175a2cfc139b6
tlplatz <tlplatz@gmail.com>
2023-03-26 13:31:05 -0500

    Framework upgrade to 4.8

--- EnigmaCipherMachine/E/Properties/Resources.Designer.cs
+++ EnigmaCipherMachine/E/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Enigma.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,14 +61,15 @@ namespace Enigma.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;Windows-1252&quot;?&gt;
+        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
         ///&lt;xs:schema xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot; xmlns:xs=&quot;http://www.w3.org/2001/XMLSchema&quot; attributeFormDefault=&quot;unqualified&quot; elementFormDefault=&quot;qualified&quot;&gt;
         ///  &lt;xsd:element name=&quot;MonthlySettings&quot;&gt;
         ///    &lt;xsd:complexType&gt;
         ///      &lt;xsd:sequence&gt;
-        ///        &lt;xsd:element name=&quot;Year&quot; type=&quot;xsd:unsignedShort&quot; /&gt;
-        ///        &lt;xsd:element name=&quot;Month&quot; type=&quot;xsd:unsignedByte&quot; /&gt;
-        ///        &lt;xsd:element name=&quot;DailySe [rest of string was truncated]&quot;;.
+        ///        &lt;xsd:element name=&quot;DailySettings&quot;&gt;
+        ///          &lt;xsd:complexType&gt;
+        ///            &lt;xsd:sequence&gt;
+        ///              &lt;xsd:element minOccurs=&quot;28&quot; maxOccurs=&quot;31&quot; name=&quot;S [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Settings {
             get {
